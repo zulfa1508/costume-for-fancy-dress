55bbbaadca5f60c71887ae59e07b8fb53dc07dc7
zulfa <fathimathzulfa1508@gmail.com>
2026-07-07 19:51:45 +0530

    message

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const API_URL = "https://costume-for-fancy-dress.onrender.com";
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://costume-for-fancy-dress.onrender.com",
+        target: API_URL,
         changeOrigin: true,
       },
       "/uploads": {
-        target: "https://costume-for-fancy-dress.onrender.com",
+        target: API_URL,
         changeOrigin: true,
       },
     },
